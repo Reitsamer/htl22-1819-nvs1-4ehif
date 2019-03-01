7d6ae1450b9d23fc4f5b04021e07475c94926f7f
Wolf Reitsamer <wr@elmar21.com>
2019-03-01 15:15:11 +0100

    Added first method to our custom module.

--- node-notes/app.js
+++ node-notes/app.js
@@ -7,3 +7,6 @@ console.log(notes);
 var alter = notes.age + 2;
 
 console.log(alter);
+
+var result = notes.add(5,7);
+console.log('Result:', result);
--- node-notes/notes.js
+++ node-notes/notes.js
@@ -1,3 +1,9 @@
 console.log('Starting notes.js');
 
-module.exports.age = 18;
+module.exports.age = 16;
+
+// add (x, y) --> x + y
+
+module.exports.add = function(x, y) {
+  return x + y;
+}
