e53d89f4df08a65dbf1c03c5842a21f6bbad3135
Wolf Reitsamer <reit@htl-donaustadt.at>
2019-05-03 15:22:18 +0200

    Added geocoding request, Added error-handling for request to darksky

--- node-weather-app/src/app.js
+++ node-weather-app/src/app.js
@@ -3,12 +3,27 @@ const request = require('request')
 const url = 'https://api.darksky.net/forecast/ace7497ac9e8602309af76a56045a170/37.8267,-122.4233?lang=de&units=si'
 
 request({ url: url, json: true }, (error, response) => {
-    // console.log(response.body)
+    if (error) {
+        console.log('Unable to connect to service.')
+        return
+    }
 
-    console.log(response.body.daily.data[0].summary)
+    if (response.body.error) {
+        console.log('Unable to find location.')
+        return
+    }
+
+     console.log(response.body.daily.data[0].summary)
     // const data = JSON.parse(response.body)
     console.log(`It is currently ${response.body.currently.temperature}. There is a ${response.body.currently.precipProbability}% chance for rain.`);
-
-    // 1. Print: It is currently 51.53 degrees. There is a 0% chance for rain.
-    // 2. Test
 })
+
+// const geocodingUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmVpdCIsImEiOiJjanVkM3F5aGUwaHk1NGRxcDZrcjlzaXdkIn0.bQG57GOwvmpb_41o5Kge_Q'
+
+// request( {url: geocodingUrl, json: true}, (error, response) => {
+//     console.log('Location:', response.body.features[0].place_name)
+//     const latitude = response.body.features[0].center[0]
+//     const longitude = response.body.features[0].center[1]
+
+//     console.log(`Latitude: ${latitude} - Longitude: ${longitude}`)
+// })
