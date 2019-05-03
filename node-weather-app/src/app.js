f6c3b39bf06c52b8cb1e1cd7d8545e8024980cbb
Wolf Reitsamer <reit@htl-donaustadt.at>
2019-05-03 15:24:58 +0200

    Added error-handling for mapbox

--- node-weather-app/src/app.js
+++ node-weather-app/src/app.js
@@ -18,12 +18,22 @@ request({ url: url, json: true }, (error, response) => {
     console.log(`It is currently ${response.body.currently.temperature}. There is a ${response.body.currently.precipProbability}% chance for rain.`);
 })
 
-// const geocodingUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmVpdCIsImEiOiJjanVkM3F5aGUwaHk1NGRxcDZrcjlzaXdkIn0.bQG57GOwvmpb_41o5Kge_Q'
+const geocodingUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/xxxLos%20Angeles.json?access_token=pk.eyJ1IjoicmVpdCIsImEiOiJjanVkM3F5aGUwaHk1NGRxcDZrcjlzaXdkIn0.bQG57GOwvmpb_41o5Kge_Q'
 
-// request( {url: geocodingUrl, json: true}, (error, response) => {
-//     console.log('Location:', response.body.features[0].place_name)
-//     const latitude = response.body.features[0].center[0]
-//     const longitude = response.body.features[0].center[1]
+request( {url: geocodingUrl, json: true}, (error, response) => {
+    if (error) {
+        console.log('Unable to connect to service.')
+        return
+    }
 
-//     console.log(`Latitude: ${latitude} - Longitude: ${longitude}`)
-// })
+    if (response.body.features.length === 0) {
+        console.log('Unable to find location.')
+        return
+    }
+    
+    console.log('Location:', response.body.features[0].place_name)
+    const latitude = response.body.features[0].center[0]
+    const longitude = response.body.features[0].center[1]
+
+    console.log(`Latitude: ${latitude} - Longitude: ${longitude}`)
+})
