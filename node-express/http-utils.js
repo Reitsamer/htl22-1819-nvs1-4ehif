1e9503fa9cb8c433118fa140ac85f039260c6ac9
Wolf Reitsamer <reit@htl-donaustadt.at>
2019-05-17 15:29:14 +0200

    finalized static http server example

---
+++ node-express/http-utils.js
@@ -0,0 +1,62 @@
+const http = require('http')
+const fs = require('fs')
+
+const contentTypes = {
+    'html': 'text/html',
+    'js': 'text/javascript',
+    'css': 'text/css',
+    'ico': 'image/x-icon',
+    'jpg': 'image/x-icon'
+}
+
+const respondToStaticRequest = (request, response) => {
+    var filename = getFilename(request)
+    var fileExtension = getExtension(filename)
+
+    var contentType = contentTypes[fileExtension]
+    if (contentType === undefined)
+    {
+        sendError(response, 400)
+        return
+    }
+
+    if (!fs.existsSync(filename)) {
+        sendError(response, 404)
+        return
+    }
+
+    sendFile(response, contentType, filename)
+}
+
+const getFilename = (request) => {
+    var filename = request.url
+    if (filename === '/')
+        filename = '/index.html'
+
+    return 'public' + filename
+}
+
+const getExtension = (filename) => {
+    var parts = filename.split('.')
+    return parts[parts.length-1]
+}
+
+const sendFile = (res, contentType, filename) => {
+    var fileContent = fs.readFileSync(filename)
+ 
+    res.statusCode = 200
+    res.contentType = contentType
+    res.write(fileContent)
+    res.end()
+}
+
+const sendError = (res, errorCode) => {
+    res.statusCode = errorCode
+    res.setHeader('Content-Type', 'text/plain')
+    res.write('Error: ' + errorCode)
+    res.end()
+}
+
+module.exports = {
+    respondToStaticRequest
+}
--- node-express/server.js
+++ node-express/server.js
@@ -1,14 +1,11 @@
 const http = require('http')
 
+const {respondToStaticRequest} = require('./http-utils')
+
 const server = http.createServer()
 
 // event
-server.on('request', (request, response) => {
-    console.log('Request received.')
-    response.writeHead(200, { 
-        'Content-Type': 'text/html'})
-    response.end('<h1>Hello World.</h1>')
-})
+server.on('request', respondToStaticRequest)
 
 // starten
 server.listen(3000, () => {
